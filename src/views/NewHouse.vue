4e4c93019156bbf173fa5677d2158917615538b1
zhoumeijing <1121880968@qq.com>
2017-11-06 16:47:54 +0800

    更改

--- src/views/NewHouse.vue
+++ src/views/NewHouse.vue
@@ -9,11 +9,11 @@
       <popup-picker :data="natureList" title="房屋性质" v-model="nature"  style="text-align:left">
       </popup-picker>
       <x-switch title="是否唯一" v-model="stringValue"></x-switch>
-      <x-button type="warn">开始计算</x-button>
+      <x-button type="warn" @click.native="calculateShui">开始计算</x-button>
   </div>
 </template>
 <script>
-import {PopupPicker, XInput, Group, Cell, XButton,XSwitch } from "vux";
+import { PopupPicker, XInput, Group, Cell, XButton, XSwitch } from "vux";
 export default {
   components: {
     PopupPicker,
@@ -23,23 +23,28 @@ export default {
     XButton,
     XSwitch
   },
-  data() {
+    data() {
     return {
-      natureList:[["普通住宅","非普通住宅"]],
-      nature:["普通住宅"],
+      natureList: [["普通住宅", "非普通住宅"]],
+      nature: ["普通住宅"],
       houseArea: 100,
-      areaPrice:10000,
-      stringValue:0
+      areaPrice: 10000,
+      stringValue: true,
+      IsCommonHouse:"",
     };
   },
-  computed: {
-   
+  methods: {
+    calculateShui() {
+      console.log(this.nature[0]);
+      this.nature[0] == "普通住宅" ? this.IsCommonHousea=1 : this.IsCommonHousea=0;
+      this.$router.push({
+        path: "/ShuifeiPay",
+        query: { totalPrice: this.houseArea*this.areaPrice,
+        IsCommonHouse:this.IsCommonHouse,
+        IsOnly:this.stringValue}
+      });
+    }
   },
-  methods:{
-    
-  }
 };
 </script>
-<style>
 
-</style>
